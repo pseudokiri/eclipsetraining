e6978246cb74455b25c33fb433d3961a77683e5f
VAELIA9 <VAELIA9@10.131.100.76>
2017-03-14 16:00:36 +0100

    Added icons

--- com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
+++ com.sii.rental.ui/src/com/sii/rental/ui/RentalAddon.java
@@ -2,15 +2,37 @@ package com.sii.rental.ui;
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
 
 import com.opcoach.training.rental.RentalAgency;
 import com.sii.rental.core.RentalCoreActivator;
 
-public class RentalAddon {
+public class RentalAddon implements RentalUIConstants{
 
 	@PostConstruct
 	public void initContext(IEclipseContext ctx) {
 		ctx.set(RentalAgency.class, RentalCoreActivator.getAgency());
+		
+		ctx.set(RENTAL_UI_IMG_REGISTRY, getLocalImageRegistry());
+		
+	}
+	
+	ImageRegistry getLocalImageRegistry() {
+		
+		Bundle b=FrameworkUtil.getBundle(getClass());
+		
+		ImageRegistry reg=new ImageRegistry();
+		
+		reg.put(IMG_CUSTOMER, ImageDescriptor.createFromURL(b.getEntry(IMG_CUSTOMER)));
+		reg.put(IMG_RENTAL, ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL)));
+		reg.put(IMG_RENTAL_OBJECT, ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL_OBJECT)));
+		reg.put(IMG_AGENCY, ImageDescriptor.createFromURL(b.getEntry(IMG_AGENCY)));
+		
+		return reg;
+		
 		
 	}
 	
